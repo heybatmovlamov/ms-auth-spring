2445bcce0ba3336eb54aba5a5e99df6581f11dcd
Sarkhan Huseynov <sarxanhuseynov521@gmail.com>
2024-10-21 12:32:58 +0400

    Add getAllUsers endpoint to retrieve a list of all users.

--- src/main/java/turing/edu/az/ms_auth/controller/UserController.java
+++ src/main/java/turing/edu/az/ms_auth/controller/UserController.java
@@ -1,4 +1,22 @@
 package turing.edu.az.ms_auth.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import turing.edu.az.ms_auth.model.dto.UserDto;
+import turing.edu.az.ms_auth.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
 }
