1f9b0b00a83332947d3abb475bd13a49eb318557
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-10-22 21:38:35 +0400

    UserDetailsServiceImpl

---
+++ src/main/java/turing/edu/az/ms_auth/jwt/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package turing.edu.az.ms_auth.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import turing.edu.az.ms_auth.dao.entity.UserEntity;
+import turing.edu.az.ms_auth.dao.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
