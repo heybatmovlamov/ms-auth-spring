d4e3d50c86538fecfd48178b3cf9a9923a6804f2
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-10-22 21:31:33 +0400

    AuthController added

--- src/main/java/turing/edu/az/ms_auth/controller/AuthController.java
+++ src/main/java/turing/edu/az/ms_auth/controller/AuthController.java
@@ -1,14 +1,24 @@
 package turing.edu.az.ms_auth.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import turing.edu.az.ms_auth.dao.entity.UserEntity;
+import turing.edu.az.ms_auth.model.dto.TokenDto;
+import turing.edu.az.ms_auth.model.dto.UserDto;
+import turing.edu.az.ms_auth.service.AuthService;
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+    private final AuthService authService;
 
-    @GetMapping("/test")
-    public String test(){
-        return "test";
+    @PostMapping("/register")
+    public UserDto register(@RequestBody UserDto userDto){
+        return authService.register(userDto);
+    }
+    @PostMapping("/login")
+    public TokenDto login(@RequestBody UserDto userDto){
+        return authService.login(userDto);
     }
 
 }
